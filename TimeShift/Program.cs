0896ae47eddc04a418e082c23570a25762725f60
Andrej Jursa <andrej.jursa@bspc.sk>
2019-05-17 14:50:46 +0200

    doplnene odstranovanie "UTC "

--- TimeShift/Program.cs
+++ TimeShift/Program.cs
@@ -23,23 +23,23 @@ namespace TimeShift
             foreach (var file in files)
             {
 
-                match = Regex.Match(file, @".*\\((.*)(\d{4}).(\d{2}).(\d{2}).(\d{2}).(\d{2}).(\d{2})(.*))", RegexOptions.IgnoreCase);
+                match = Regex.Match(file, @".*\\((.*?)(UTC\s){0,1}(\d{4}).(\d{2}).(\d{2}).(\d{2}).(\d{2}).(\d{2})(.*))", RegexOptions.IgnoreCase);
                 if (match.Success)
                 {
                     var dt = new DateTime(
-                        int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value), int.Parse(match.Groups[5].Value),
-                        int.Parse(match.Groups[6].Value), int.Parse(match.Groups[7].Value), int.Parse(match.Groups[8].Value));
+                        int.Parse(match.Groups[4].Value), int.Parse(match.Groups[5].Value), int.Parse(match.Groups[6].Value),
+                        int.Parse(match.Groups[7].Value), int.Parse(match.Groups[8].Value), int.Parse(match.Groups[9].Value));
                     dt = dt.AddHours(signum * shiftHours);
                     dt = dt.AddMinutes(signum * shiftMinutes);
                     dt = dt.AddSeconds(signum * shiftSeconds);
-                    Console.WriteLine("ren \"{0}\" \"{1}\"", match.Groups[1].Value, match.Groups[2].Value + dt.ToString("yyyy-MM-dd_HH.mm.ss") + match.Groups[9].Value);
+                    Console.WriteLine("ren \"{0}\" \"{1}\"", file, match.Groups[2].Value + dt.ToString("yyyy-MM-dd_HH.mm.ss") + match.Groups[10].Value);
                 }
             }
 
             Console.WriteLine(@"");
             Console.WriteLine(@"");
-            Console.WriteLine(@"Press Enter.");
-            Console.ReadLine();
+            //Console.WriteLine(@"Press Enter.");
+            //Console.ReadLine();
         }
 
         static void Usage()
